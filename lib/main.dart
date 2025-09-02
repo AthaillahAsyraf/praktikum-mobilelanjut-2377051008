74319a669b7bb7fab5f8ae785600a2f2c87f9b7e
athaa0725 <athaa0725@gmail.com>
2025-09-02 05:18:49 +0000

    pertemuan ke-2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Athaillah Asyraf'),
+      home: const MyHomePage(title: 'Athaillah Asyraf - 2377051008'),
     );
   }
 }
